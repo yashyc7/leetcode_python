d15c6fcb54953c42acb825b9503c4d5431547566
Yash Chauhan <yashc9411407582@gmail.com>
2026-03-09 01:57:13 -0700

    Time: 13 ms (64.8%), Space: 20 MB (61.38%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,26 +5,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        low = 0  # starting the lowest pointer at first element of the array
-        high = 0  # starting the high pointer also at the first element of the array 
-        result = float('inf')  # we want minimum length, so start with +infinity
-        n = len(nums)
-        sum = 0  # suppose default sum is zero 
+        low =0 
+        total = 0 
+        ans = float('inf')
+        
+        for high in range(len(nums)): 
 
-        while (high < n):  # high must not pass length of the array 
-            sum = sum + nums[high]  # first window sum 
+            #include the information 
 
-            while (sum >= target):  # while sum is greaterthan equal to target then eliminate them untill we get smallest subarray size
-                length = high - low + 1  # for eg size of array 0,2 is litteraly 2-0+1 =3 as 0,1,2
-                result = min(length, result)  # take minimum length
+            total += nums[high]
 
-                # now update the sum after result calculated we must proceed to elimintate things untill we get optimal size
-                sum = sum - nums[low]
-
-                # now remove / fire the old ones since we can still acheive to target
-                low = low + 1  # shrink the window
-            
-            # outside of the inner while loop increase the size of the windows 
-            high = high + 1 
-
-        return 0 if result == float('inf') else result
+            # while information is correct then shrink 
+            while(total>=target): 
+                length = high-low+1
+                ans = min(ans,length)
+                #now shrinking the window
+                total = total - nums[low]
+                low = low+1
+        return 0 if ans == float('inf') else ans
