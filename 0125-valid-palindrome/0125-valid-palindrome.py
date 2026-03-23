796bdc74909c0ce63422012da45a0e3470f0062f
Yash Chauhan <yashc9411407582@gmail.com>
2026-03-23 19:28:31 +0530

    Time: 12 ms (15.38%), Space: 19.6 MB (88.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        s=s.lower()
-        temp=""
-        alpha="abcdefghijklmnopqrstuvwxyz1234567890"
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_str = ""
+        #cleaning string 
         for i in s:
-            if(i in alpha):
-                temp+=i
-        return (temp[::1]==temp[::-1])
+            if i.isalnum():
+                cleaned_str  = cleaned_str+i.lower()
+
+        i =  0
+        j = len(cleaned_str)-1
+
+        while(i<j):
+            if cleaned_str[i]!=cleaned_str[j]:
+                return False
+            i=i+1
+            j=j-1
+        return True
