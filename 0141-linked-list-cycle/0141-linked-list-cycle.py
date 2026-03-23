b1f3fceb30dc3579602986abdf486e590a96d840
Yash Chauhan <yashc9411407582@gmail.com>
2026-03-23 21:32:52 +0530

    Time: 60 ms (19.89%), Space: 22.6 MB (43.64%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,17 +6,14 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # i am using the slow and the fast pointer approach in this question so 
-        # starting with the slow and fast pointers
+        
+        slow,fast = head, head 
 
-        slow = head
-        fast = head 
-        while (fast!=None) and (fast.next!=None):
-            # move the slow pointer one time and then 
-            # move the fast pointer two times for detecting cycle 
+
+        while(fast and fast.next):
             slow = slow.next
-            fast = fast.next.next
-
-            if slow  == fast : 
-                return True # if the pointeres meet at place then it mean the cycle has been found then return the true
-        return False # otherwise return false 
+            fast = fast.next
+            fast = fast.next
+            if slow==fast:
+                return True
+        return False
