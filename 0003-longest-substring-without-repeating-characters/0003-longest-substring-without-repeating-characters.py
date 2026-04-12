7058022e4eaa2a1991cde10dd587ddaf620f331d
Yash Chauhan <yashc9411407582@gmail.com>
2026-04-12 21:28:55 +0530

    Time: 15 ms (42.6%), Space: 19.3 MB (43.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,31 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        #it just only means that len(freq) is equal to the length of the window 
+        # THIS Proves that every character has only one occurence 
 
-        #if the length of the longest substring is asked then now 
-        # without duplicate meaning each character has exactly one occurence 
-        # hence length of the hashmap should be equal to the length of the current_window 
+        low = 0
+        ans = float('-inf')
+        freq = {}
 
+        for high in range(len(s)):
+            freq[s[high]]=freq.get(s[high],0)+1
 
-        low = 0 
-        freq = { }
-        ans = 0
-
-
-        for high in range(len(s)): 
-
-            # include information in window 
-
-            freq[s[high]] = freq.get(s[high], 0) + 1
-
-            while (len(freq)<high-low+1): #since high-low+1 is the answer to be returned uuntill this is incorrect we would shrink 
-                freq[s[low]]=freq[s[low]]-1 #shrink then outside while update ans 
-
+            while(freq[s[high]]>1) :
+                freq[s[low]]=freq[s[low]]-1
                 if freq[s[low]]==0:
                     del freq[s[low]]
-                
                 low = low + 1
-
-            if len(freq)==(high-low+1): # update answer with information is right check 
-                ans = max(ans,high-low+1)
-
-        return ans
+            length = high - low + 1
+            ans = max(ans,length)
+        return 0 if ans == float('-inf') else ans 
