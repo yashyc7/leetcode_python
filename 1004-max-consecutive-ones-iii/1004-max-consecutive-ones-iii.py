f9c513a24a70eb02b693b3f167db1c6fd6b5aa6d
Yash Chauhan <yashc9411407582@gmail.com>
2026-04-12 21:47:46 +0530

    Time: 118 ms (5.06%), Space: 22.3 MB (30.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -7,7 +7,7 @@ class Solution:
         for high in range (len(nums)):
             freq[nums[high]]= freq.get(nums[high],0)+1
 
-            while(freq.get(0,0)>k):
+            while(freq.get(0,0)>k): # if the freq of 0 is greater than k in window then shrink the window to fit 
                 freq[nums[low]]=freq[nums[low]]-1
                 if freq[nums[low]]==0:
                     del freq[nums[low]]
