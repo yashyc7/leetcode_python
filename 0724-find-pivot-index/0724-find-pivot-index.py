aaf1c8ebfb0d2c8c612b705167ca87398b6a0fe2
Yash Chauhan <yashc9411407582@gmail.com>
2026-06-14 10:46:56 +0530

    Time: 4 ms (74.26%), Space: 13.2 MB (86.07%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,14 +1,17 @@
 class Solution(object):
     def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0 # left represents the prefix sum 
         total = sum(nums)
-        left = 0
 
         for i in range(len(nums)):
+           
             right = total - left - nums[i]
 
-            if left == right:
-                return i
-
-            left += nums[i]
-
-        return -1
+            if left == right : 
+                return i 
+            left = left + nums[i]
+        return -1 
