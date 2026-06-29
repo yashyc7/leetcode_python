c50bd7f3c78548c5adee21d1fcd56e078136e000
Yash Chauhan <yashc9411407582@gmail.com>
2026-06-29 16:33:58 +0530

    Time: 8 ms (43.25%), Space: 13.3 MB (56.02%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,14 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left = 0 # left represents the prefix sum 
+        # concept 
+
+        ## the left part of the index i in array is called prefix and the right part of the index is called as the suffix 
+
+        # the array is represented like as below 
+
+        #  0 , arr[i-x].....arr[i-1]  , arr[i] , arr[i+1], arr[i+2] .. arr[n-1]
+
+        # so therefore the sum of the array is like  sum = prefix[i]+suffix[i]+arr[i]
+
+        # here in this question lets say prefix[i] is termed as left and suffix is renamed as the right 
+
+        left = 0 
         total = sum(nums)
 
         for i in range(len(nums)):
-           
-            right = total - left - nums[i]
+            right = total-left-nums[i]
 
-            if left == right : 
+            if  left == right : 
                 return i 
-            left = left + nums[i]
+            left += nums[i]
         return -1 
+
